b6f05dd25260f6eb29b4030fba08a41ab38a5a70
sobujprocoder <sobuj@procoder.org>
2025-04-05 12:39:40 +0600

    First Project Created
    First Project Created with a Route

---
+++ application/views/about/about.php
@@ -0,0 +1,14 @@
+<?php
+    defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello About Page</title>
+</head>
+<body>
+    <h1>Hello About Page</h1>
+</body>
+</html>
